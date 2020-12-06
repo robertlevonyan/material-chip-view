7e423856c5d4fedb7696e011654a8692023568a4
Robert <bigbob1991@gmail.com>
2020-12-06 11:58:34 +0400

    updated to sdk30

--- Chip/app/src/main/java/com/robertlevonyan/views/chip/Chip.kt
+++ Chip/app/src/main/java/com/robertlevonyan/views/chip/Chip.kt
@@ -98,9 +98,9 @@ public class Chip : AppCompatTextView {
     var onSelectClickListener: OnSelectClickListener? = null
     var onIconClickListener: OnIconClickListener? = null
 
-    constructor(context: Context?) : this(context, null, 0)
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    constructor(context: Context) : this(context, null, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         initTypedArray(attrs)
         buildView()
         setSingleLine()
